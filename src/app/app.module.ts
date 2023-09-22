a5e0525af5508c01ebbfff0758ed210360fba36a
Fabiecito <josforero@uniboyaca.edu.co>
2023-09-22 08:58:29 -0500

    formulario hecho validar edad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { FormularioComponent } from './formulario/formulario.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+  formulario: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.formulario = this.fb.group({
+      nombre: ['', Validators.required],
+      edad: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  verificarEdad() {
+    if (this.formulario.valid) {
+      const edad = this.formulario.get('edad')?.value;
+      if (edad !== null && edad !== undefined) {
+        const esMayorEdad = edad >= 18;
+  
+        if (esMayorEdad) {
+          alert('El usuario es mayor de edad');
+        } else {
+          alert('El usuario es menor de edad');
+        }
+      }
+    }
+  }
 }
